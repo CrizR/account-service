3a46fc3ee3aec1dbb6404222bbb099e7547b76db
brett adcock <brettadcock@bretts-MacBook-Pro.local>
2018-07-29 19:39:58 -0400

    middleware folder

--- models/account.go
+++ models/account.go
@@ -11,16 +11,16 @@ const (
 
 // Account represents details for a particular user account.
 type Account struct {
-	ID          int
-	AccountType AccountType
-	Email       string
-	Password    string
-	FirstName   string
-	LastName    string
-	Bio         string
-	Industry    string
-	Education   string
-	State       string
-	Reputation  int
-	Interests   []string
+	ID          int         `json:"id"`
+	AccountType AccountType `json:"account_type"`
+	Email       string      `json:"email"`
+	Password    string      `json:"password"`
+	FirstName   string      `json:"first_name"`
+	LastName    string      `json:"last_name"`
+	Bio         string      `json:"bio"`
+	Industry    string      `json:"industry"`
+	Education   string      `json:"education"`
+	State       string      `json:"state"`
+	Reputation  int         `json:"reputation"`
+	Interests   []string    `json:"interests"`
 }
