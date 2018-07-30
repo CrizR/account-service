d59970883ab9ca7ac2258d3ef23f2a2867ea1560
brett adcock <brettadcock@bretts-MacBook-Pro.local>
2018-07-29 21:09:56 -0400

    first pass at handlers

--- server/handlers.go
+++ server/handlers.go
@@ -1,29 +1,71 @@
 package server
 
 import (
+	"log"
+	"net/http"
+
+	"github.com/ecclesia-dev/account-service/models"
 	"github.com/labstack/echo"
 )
 
+const (
+	indent = "  "
+)
+
 func (s *Server) createAccount(ctx echo.Context) error {
-	return nil
+
+	var account models.Account
+	if err := ctx.Bind(&account); err != nil {
+		log.Fatal(err)
+	}
+	// Add validation call here
+	if err := s.accounts.CreateAccount(account); err != nil {
+		log.Fatal(err)
+	}
+
+	return ctx.JSON(http.StatusOK, nil)
 }
 
 func (s *Server) getAllAccounts(ctx echo.Context) error {
-	return nil
+	accounts, err := s.accounts.GetAllAccounts()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return ctx.JSONPretty(http.StatusOK, accounts, indent)
 }
 
 func (s *Server) getAccountByEmail(ctx echo.Context) error {
-	return nil
+	account, err := s.accounts.GetAccountByEmail(ctx.QueryParam("email"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	return ctx.JSONPretty(http.StatusOK, account, indent)
 }
 
 func (s *Server) getAccountByID(ctx echo.Context) error {
-	return nil
+	account, err := s.accounts.GetAccountByID(ctx.QueryParam("id"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	return ctx.JSONPretty(http.StatusOK, account, indent)
 }
 
 func (s *Server) removeAccount(ctx echo.Context) error {
-	return nil
+	err := s.accounts.RemoveAccount(ctx.QueryParam("id"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	return ctx.JSON(http.StatusOK, nil)
 }
 
 func (s *Server) updateAccount(ctx echo.Context) error {
-	return nil
+
+	var update map[string]interface{}
+	if err := ctx.Bind(&update); err != nil {
+		log.Fatal(err)
+	}
+	if err := s.accounts.UpdateAccount(ctx.QueryParam("id"), nil); err != nil {
+		log.Fatal(err)
+	}
+	return ctx.JSON(http.StatusOK, nil)
 }
